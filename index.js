2dce82c3738d1c05d92caaf9e937c7741ec33c34
Gbenga Omowole <omowole.gbenga@gmail.com>
2019-10-02 23:31:14 +0000

    fix: ui enhancements

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const file =  document.getElementById('file')
-const model = document.getElementById('model')
-const placeholder = document.getElementById('placeholder')
+import { formatBytes } from "./helper.js";
 
+const fileInput = document.getElementById("fileInput");
+const model = document.getElementById("model");
+const modelWrapper = document.getElementById("modelWrapper");
+const placeholder = document.getElementById("placeholder");
+const infoTag = document.getElementById("infoTag");
+const removeModel = document.getElementById('removeModel')
 
-file.onchange = (evt) => {
-    const file = evt.target.files[0]
-    model.src = URL.createObjectURL(file)
-    model.alt = file.name
-    placeholder.classList.add('hidden')
-    model.classList.remove('hidden')
+fileInput.onchange = (evt) => {
+  const file = evt.target.files[0];
+  model.src = URL.createObjectURL(file);
+  model.alt = file.name;
+  placeholder.classList.add("hidden");
+  modelWrapper.classList.remove("hidden");
+  infoTag.children[0].innerHTML = 
+  `
+  <li>${file.name}</li>
+  <li>${formatBytes(file.size)}</li>
+  `;
 };
+
+removeModel.click = () => {
+    placeholder.classList.remove("hidden");
+    modelWrapper.classList.add("hidden");
+    infoTag.children[0].innerHTML = ''
+}
